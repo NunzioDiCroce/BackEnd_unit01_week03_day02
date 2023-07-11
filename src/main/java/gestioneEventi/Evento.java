4dd7b9999d20995d95d35d8db5df70ffd942a504
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-11 16:09:07 +0200

    Commit, enum "TipoEvento" created

--- src/main/java/gestioneEventi/Evento.java
+++ src/main/java/gestioneEventi/Evento.java
@@ -1,5 +1,14 @@
 package gestioneEventi;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "eventi")
+
 public class Evento {
 
 	// id (chiave primaria)
@@ -8,6 +17,11 @@ public class Evento {
 	// descrizione
 	// tipoEvento [PUBBLICO, PRIVATO]
 	// numeroMassimoPartecipanti
+
+	@Id
+	private long id;
+	private String titolo;
+	private LocalDate dataEvento;
 
 	// costruttori, getters e setters...
 
