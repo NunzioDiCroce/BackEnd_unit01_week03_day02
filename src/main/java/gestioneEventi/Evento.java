1f97877b6313be4168aaa1c2ec5b819c555689a1
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-11 15:59:55 +0200

    Commit, class "Evento" work in progress

--- src/main/java/gestioneEventi/Evento.java
+++ src/main/java/gestioneEventi/Evento.java
@@ -2,4 +2,13 @@ package gestioneEventi;
 
 public class Evento {
 
+	// id (chiave primaria)
+	// titolo
+	// dataEvento
+	// descrizione
+	// tipoEvento [PUBBLICO, PRIVATO]
+	// numeroMassimoPartecipanti
+
+	// costruttori, getters e setters...
+
 }
