2f543d7dced78fd97dc462ee89ffd855a80fbc45
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-11 19:41:58 +0200

    Commit, class "App" work in progress

--- src/main/java/gestioneEventi/App.java
+++ src/main/java/gestioneEventi/App.java
@@ -1,8 +1,34 @@
 package gestioneEventi;
 
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import utils.JpaUtil;
+
 public class App {
 
+	private static EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+
 	public static void main(String[] args) {
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		System.out.println("");
+		System.out
+				.println("- - - - - - - - - - - - - - - App Gestione Eventi is running - - - - - - - - - - - - - - -");
+
+		// public Evento(String _titolo, LocalDate _dataEvento, String _descrizione,
+		// TipoEvento _tipoEvento,
+		// int _numeroMassimoPartecipanti)
+		LocalDate primaData = LocalDate.of(2023, 7, 1);
+		Evento primoEvento = new Evento("primoEvento", primaData, "primaDescrizione", TipoEvento.PRIVATO, 100);
+
+		EventoDAO eventoDao = new EventoDAO(entityManager);
+
+		// - - - - - - - - - - - - - - - - - - - - SAVE
+		eventoDao.save(primoEvento);
 
 	}
 
